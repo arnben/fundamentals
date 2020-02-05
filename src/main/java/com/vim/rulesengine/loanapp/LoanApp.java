2da0cd8f3430cffdc67b1339cc769038be604ef5
Masurkar <vineshmasurkar@yahoo.com>
2020-02-05 15:22:19 -0500

    Adds Spring Rules Engine.

---
+++ src/main/java/com/vim/rulesengine/loanapp/LoanApp.java
@@ -0,0 +1,62 @@
+package com.vim.rulesengine.loanapp;
+
+import java.util.Arrays;
+
+public class LoanApp {
+    private String firstName;
+    private String lastName;
+    private double income;
+    private double expences;
+    private String stateCode;
+    private String status;
+
+    public void setStatus(String status) {
+        if(! LoanState.contains(status))
+            throw new IllegalArgumentException("invalid status:" + status);
+        this.status = status;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public double getIncome() {
+        return income;
+    }
+
+    public void setIncome(double income) {
+        this.income = income;
+    }
+
+    public double getExpences() {
+        return expences;
+    }
+
+    public void setExpences(double expences) {
+        this.expences = expences;
+    }
+
+    public String getStateCode() {
+        return stateCode;
+    }
+
+    public void setStateCode(String stateCode) {
+        this.stateCode = stateCode;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+}
---
+++ src/main/java/com/vim/rulesengine/loanapp/LoanState.java
@@ -0,0 +1,23 @@
+package com.vim.rulesengine.loanapp;
+
+public enum LoanState {
+    INVALID_STATE("Sorry we are not doing business in your state"),
+    INVALID_INCOME_EXPENSE_RATIO("Sorry we cannot provide the loan given this expense/income ratio"),
+    APPROVED("Your application has been approved"),
+    INSUFFICIENT_DATA("You did not provide enough information on your application"),
+    INPROGRESS("in progress");
+
+    private String status;
+
+    private LoanState(String msg) {
+        this.status = msg;
+    }
+
+    public static boolean contains(String status) {
+        return (null == LoanState.valueOf(status)) ? false : true;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+}
